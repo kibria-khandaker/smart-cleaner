816014f36f718eab12c7d389ec64c05a5063262c
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-17 03:18:36 +0600

    SignIn firebase Auth function Done, Able to User Login

---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from "react-router-dom";
+import { auth } from '../../firebase.init';
+import logo from '../../Smart-Cleaner.png';
+import CustomLink from '../CustomLink/CustomLink';
+
+
+const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="light" variant="info">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={40} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <p className='m-0 '>I'm a smart cleaner. able to gift a clean life.</p>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={CustomLink} to="/">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/about">About</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/blog"> Blog </Nav.Link>
+
+                            {
+                                user
+                                    ?
+                                    <Nav.Link onClick={handleSignOut} as={CustomLink} to="/login">
+                                        Sign Out
+                                    </Nav.Link>
+                                    :
+                                    <Nav.Link as={CustomLink} to="/login">
+                                        Login
+                                    </Nav.Link>
+                            }
+
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
